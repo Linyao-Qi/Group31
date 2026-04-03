aa7c508925ae763d430c0850674dbc980a063f4d
Star-Angle <1321931904@qq.com>
2026-04-03 16:54:37 +0800

    Update MO job posting and applicant hiring functionality

--- V1.0/Application.java
+++ V1.0/Application.java
@@ -1,23 +1,14 @@
 import java.io.Serializable;
 
-/**
- * 申请实体类 - 对应TA的岗位申请，MO基于此进行录用操作
- */
 public class Application implements Serializable {
     private static final long serialVersionUID = 1L;
-    // 申请唯一ID
     private String appId;
-    // 关联岗位ID
     private String jobId;
-    // 申请者TAID
     private String taId;
-    // 申请状态：PENDING(待审核)、ACCEPTED(录用)、REJECTED(拒绝)
     private String appStatus;
 
-    // 无参构造
     public Application() {}
 
-    // 全参构造
     public Application(String appId, String jobId, String taId, String appStatus) {
         this.appId = appId;
         this.jobId = jobId;
@@ -25,7 +16,6 @@ public class Application implements Serializable {
         this.appStatus = appStatus;
     }
 
-    // Getter & Setter
     public String getAppId() { return appId; }
     public void setAppId(String appId) { this.appId = appId; }
     public String getJobId() { return jobId; }
@@ -35,14 +25,8 @@ public class Application implements Serializable {
     public String getAppStatus() { return appStatus; }
     public void setAppStatus(String appStatus) { this.appStatus = appStatus; }
 
-    // 重写toString
     @Override
     public String toString() {
-        return "Application{" +
-                "appId='" + appId + '\'' +
-                ", jobId='" + jobId + '\'' +
-                ", taId='" + taId + '\'' +
-                ", appStatus='" + appStatus + '\'' +
-                '}';
+        return "Application{appId='" + appId + "', jobId='" + jobId + "', taId='" + taId + "', status=" + appStatus + "}";
     }
 }
--- V1.0/Job.java
+++ V1.0/Job.java
@@ -1,25 +1,15 @@
 import java.io.Serializable;
 
-/**
- * 岗位实体类 - 对应MO发布的TA岗位
- */
 public class Job implements Serializable {
     private static final long serialVersionUID = 1L;
-    // 岗位唯一ID
     private String jobId;
-    // 发布者（MO）ID
     private String moId;
-    // 岗位名称（如：课程TA、监考助理）
     private String jobName;
-    // 岗位要求
     private String jobRequirements;
-    // 岗位状态：OPEN(开放)、CLOSED(关闭)
     private String jobStatus;
 
-    // 无参构造（JSON反序列化必需）
     public Job() {}
 
-    // 全参构造
     public Job(String jobId, String moId, String jobName, String jobRequirements, String jobStatus) {
         this.jobId = jobId;
         this.moId = moId;
@@ -28,7 +18,6 @@ public class Job implements Serializable {
         this.jobStatus = jobStatus;
     }
 
-    // Getter & Setter
     public String getJobId() { return jobId; }
     public void setJobId(String jobId) { this.jobId = jobId; }
     public String getMoId() { return moId; }
@@ -40,15 +29,8 @@ public class Job implements Serializable {
     public String getJobStatus() { return jobStatus; }
     public void setJobStatus(String jobStatus) { this.jobStatus = jobStatus; }
 
-    // 重写toString
     @Override
     public String toString() {
-        return "Job{" +
-                "jobId='" + jobId + '\'' +
-                ", moId='" + moId + '\'' +
-                ", jobName='" + jobName + '\'' +
-                ", jobRequirements='" + jobRequirements + '\'' +
-                ", jobStatus='" + jobStatus + '\'' +
-                '}';
+        return "Job{jobId='" + jobId + "', moId='" + moId + "', jobName='" + jobName + "', status=" + jobStatus + "}";
     }
 }
