62031f7b7858a68d1fc78743fd76652f0ebb6190
LQmc-7 <lqmcpoohpooh@outlook.com>
2026-04-08 20:14:41 +0800

    add profile model class

--- liqimochu/src/model/Profile.java
+++ liqimochu/src/model/Profile.java
@@ -7,22 +7,23 @@ public class Profile {
     private String skills;
     private String major;
     private String cvPath;
-    
-    public Profile(String name, String id, String email, String skills, String major, String cvPath) {
+
+    public Profile(String name, String id, String email,
+                   String skills, String major, String cvPath) {
         this.name = name;
         this.id = id;
         this.email = email;
         this.skills = skills;
-         this.major = major;
-         this.cvPath = cvPath;
-        }
-        
+        this.major = major;
+        this.cvPath = cvPath;
+    }
+
     public String getName() { return name; }
     public String getId() { return id; }
     public String getEmail() { return email; }
     public String getSkills() { return skills; }
     public String getMajor() { return major; }
     public String getCvPath() { return cvPath; }
-    
+
     public void setCvPath(String cvPath) { this.cvPath = cvPath; }
 }
